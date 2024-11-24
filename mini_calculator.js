8f76afe21b5168035ba74927d257a65c66b73787
Yin Thiri Mon <yinthirimon@gmail.com>
2024-11-25 01:43:06 +0630

    added: change result to temp storage

--- mini_calculator.js
+++ mini_calculator.js
@@ -4,6 +4,7 @@ let temp = ""
 function insert(para){
     if(para == 'calculate'){
         result.innerHTML = eval(temp)    //calculate with math operation from string
+        temp = eval(temp)   //change result to temp storage
     }else{
         temp += para.innerHTML;     //store all user data
         result.innerHTML = temp     // to see user number
